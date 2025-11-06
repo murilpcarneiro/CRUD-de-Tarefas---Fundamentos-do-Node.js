2f6408eaecfd476314599d1be9cb0fb138a2fb63
Murilo <murilpcarneiro@gmail.com>
2025-11-06 16:25:59 -0300

    fix: update task retrieval to use query parameters and implement task update functionality

--- src/routes.js
+++ src/routes.js
@@ -33,7 +33,7 @@ export const routes = [
     method: "GET",
     path: buildRoutePath("/tasks"),
     handler: (req, res) => {
-      const { title, description } = req.params;
+      const { title, description } = req.query;
       const tasks = database.select(
         "tasks",
         title ? { title } : description ? { description } : null
@@ -44,7 +44,22 @@ export const routes = [
   {
     method: "PUT",
     path: buildRoutePath("/tasks/:id"),
-    handler: (req, res) => {},
+    handler: (req, res) => {
+      const { id } = req.params;
+      const { title, description } = req.body;
+      const tasks = database.select("tasks", { id });
+      if (!tasks || tasks.length === 0) {
+        return res.writeHead(404).end();
+      }
+      const task = tasks[0];
+      database.update("tasks", id, {
+        ...task,
+        title,
+        description,
+        updated_at: new Date().toISOString(),
+      });
+      return res.writeHead(204).end();
+    },
   },
   {
     method: "DELETE",
