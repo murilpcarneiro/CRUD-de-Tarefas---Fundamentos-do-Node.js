b765e9943ea85da18f431cead13fd93345e59fc9
Murilo <murilpcarneiro@gmail.com>
2025-11-06 15:39:29 -0300

    refactor routes to use buildRoutePath utility for URL generation

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,36 @@
+import { buildRoutePath } from "./utils/build-route-path.js";
+
 export const routes = [
   {
     method: "GET",
-    url: "/",
+    url: buildRoutePath("/"),
     handler: (req, res) => {
       res.end("Hello World");
     },
   },
   {
     method: "POST",
-    url: "/tasks",
+    url: buildRoutePath("/tasks"),
+    handler: (req, res) => {},
+  },
+  {
+    method: "GET",
+    url: buildRoutePath("/tasks"),
+    handler: (req, res) => {},
+  },
+  {
+    method: "PUT",
+    url: buildRoutePath("/tasks/:id"),
+    handler: (req, res) => {},
+  },
+  {
+    method: "DELETE",
+    url: buildRoutePath("/tasks/:id"),
+    handler: (req, res) => {},
+  },
+  {
+    method: "PATCH",
+    url: buildRoutePath("/tasks/:id/complete"),
     handler: (req, res) => {},
   },
 ];
